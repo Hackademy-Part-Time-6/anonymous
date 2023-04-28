0c38e7f1c48564b6c82cf0e584630696ac8ed6d0
laptopjohn <lebenper@gmail.com>
2023-04-28 18:28:28 +0200

    componente create-ad

---
+++ resources/views/ad/create.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot name='title'>Anonymous - vende algo interesante</x-slot>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        nuevo anuncio
+                    </div>
+                    <div class="card-body">
+                        <livewire:create-ad/>
+                    </div>
+                </div>
+             </div>
+        </div>
+    </div>
+
+<div>
+    <h1>Create new Ad</h1>
+</div>
+
+</x-layout>
