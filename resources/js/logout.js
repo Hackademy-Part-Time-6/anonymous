cc1a1ebb0f54f3b7abd54fa2e4ba7f17812c0c05
LuberGonzalez <luber.gonzalez.77@gmail.com>
2023-04-28 09:58:27 +0200

    Se corrigio el archivo logout.js para poder cerrar sesion en el sistema.

--- resources/js/logout.js
+++ resources/js/logout.js
@@ -1,8 +1,8 @@
 const $logoutBtn = document.getElementById("logoutBtn");
 
-if($logout) {
-    document.addEventListener("click",(e)=> {
-        // alert("HOLA");
+if(logoutBtn) {
+    $logoutBtn.addEventListener("click",(e)=> {
+        alert("HOLA");
         e.preventDefault();
         const $formLogin = document.getElementById('logoutForm').submit(); 
     });
