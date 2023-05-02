27a2394411d397bebfac5064be2fa94c16a28d28
LuberGonzalez <luber.gonzalez.77@gmail.com>
2023-05-02 19:08:12 +0200

    Correccion de errores

--- resources/views/ad/show.blade.php
+++ resources/views/ad/show.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row my-5">
             <div class="col-12 col-md-6">
-                <div id="adImages" classÇ="carousel slide" data-bs-ride="true">
+                <div id="adImages" class="carousel slide" data-bs-ride="true">
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#adimages" data-bs-slide-to="0" class="active" aria-current="true" aria-label="slide 1"></button>
                         <button type="button" data-bs-target="#adimages" data-bs-slide-t0="1" aria-label="slide 2"></button>
@@ -21,23 +21,23 @@
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#adimages" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">previous</span>
+                <span class="visually-hidden">Anterior</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#adimages" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">next</span>
+                <span class="visually-hidden">Siguiente</span>
             </button>
         </div>
         </div>
        
         <div class="col-12 col-md-6">
-        <div><b> título:</b>{{ $ad->title }}</div>
-        <div><b> precio:</b>{{ $ad->price }}</div>
-        <div><b> descripción:</b>{{ $ad->body }}</div>
-        <div><b> publicado el:</b>{{ $ad->created_at->format('d/m/y') }}</div>
-        <div><b> por:</b>{{ $ad->user->name }}</div>
-        <div><a href="{{ route('category. ads', $ad->category) }}">#{{ $ad->category->name }}</a></div>
-        <div><a href="" class="btn btn-success">comprar</a></div>
+        <div><b> Título: </b>{{ $ad->title }}</div>
+        <div><b> Precio: </b>{{ $ad->price }}</div>
+        <div><b> Descripción: </b>{{ $ad->body }}</div>
+        <div><b> Publicado el: </b>{{ $ad->created_at->format('d/m/y') }}</div>
+        <div><b> Por:</b>{{ $ad->user->name }}</div>
+        <div><a href="{{ route('category.ads', $ad->category) }}">#{{ $ad->category->name }}</a></div>
+        <div><a href="" class="btn btn-success">Comprar</a></div>
         </div>
     </div>
     </div>
