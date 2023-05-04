43ac2a1bff58a9629d3e6b5e87ea904bd527c082
LuberGonzalez <luber.gonzalez.77@gmail.com>
2023-05-04 13:08:01 +0200

    Solucion de errores en la vista de RevisorController

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\BecomeRevisor;
 use App\Models\Ad;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class RevisorController extends Controller
 {
@@ -34,10 +37,10 @@ class RevisorController extends Controller
         return redirect()->back()->withMessage(['type' => 'danger', 'text' => 'Anuncio rechazado']);
     }
 
-
     public function becomeRevisor()
     {
-        Mail::to('admin@rapido.es')->send(new BecomeRevisor(Auth::user()));
-        return redirect()->route('home')->withMessage(['type' => 'success', 'text' => 'Solicitud enviada con éxito, pronto sabrás algo, gracias!']);
-    }
+        Mail::to('admin@anonymous.es')->send(new BecomeRevisor(Auth::user()));
+        $ads = Ad::where('is_accepted', true)->orderBy('created_at', 'desc')->take(6)->get();
+        return redirect()->route('home')->with(['ads' => $ads, 'message' => ['type' => 'success', 'text' => 'Solicitud enviada con éxito, pronto sabrás algo, gracias!']]);
+    }    
 }
