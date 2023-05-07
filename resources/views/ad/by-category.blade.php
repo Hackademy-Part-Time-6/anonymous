87037078559fd9f8e32a5f73443356000a794afa
laptopjohn <lebenper@gmail.com>
2023-05-07 23:55:13 +0200

    traduccion home,footer y by-category

--- resources/views/ad/by-category.blade.php
+++ resources/views/ad/by-category.blade.php
@@ -40,7 +40,7 @@
             <div class="text-center">
                 <h2>{{ __('Uyy.. parece que no hay nada') }}</h2>
                 <img src="{{ asset('./image/logo.png') }}" alt="Logo Anonymous" width="650px" height="150px">
-                <div class="text-center">
+                <div class="text-center my-5">
                     <a href="{{ route('ads.create') }}" class="btn btn-warning">{{ __('Vende tu primer objeto') }}</a>
                     <a href="{{ route('home') }}" class="btn btn-warning">{{ __('Vuelve a la home') }}</a>
                 </div>
