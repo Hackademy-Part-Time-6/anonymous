f29d3f63084c80cd9ff5f37590840bab48c17ee5
laptopjohn <lebenper@gmail.com>
2023-05-07 05:45:46 +0200

    traducida la pagina de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,22 +8,20 @@
             <div class="col-12 col-md-6 offset-md-3 ">
                 {{-- Form Title --}}
 
-                <h2 class="form-title space-around">Iniciar Sesiòn</h2>
+                <h2 class="form-title space-around">{{ __('Iniciar sesión') }}</h2>
 
                 @isset($error)
-                @if ($error->any())
-                    
-                <div class="alert alert-danger">
-                    <ul>
+                    @if ($error->any())
+                        <div class="alert alert-danger">
+                            <ul>
 
-                        @foreach ($errors->all() as $error)
-                            <li>{{$error}}</li>
-                        @endforeach
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
 
-                    </ul>
-                </div>
-            
-            @endif
+                            </ul>
+                        </div>
+                    @endif
                 @endisset
 
                 {{-- Form Fields --}}
@@ -34,31 +32,36 @@
                     {{-- Email --}}
 
                     <div class="space-around my-3 ">
-                        <input type="email"  name="email" class="form-control forms_field-input " id="email" placeholder="Tu correo" data-rule="minlen:4" data-msg="Por favor cuatros carateres" />
+                        <input type="email" name="email" class="form-control forms_field-input " id="email"
+                            placeholder="{{ __('Tu correo') }}" data-rule="minlen:4"
+                            data-msg="Por favor cuatros carateres" />
                         <div class="validate"></div>
                     </div>
 
-                        {{-- Password --}}
- 
-                        <div class="space-around my-3">
+                    {{-- Password --}}
 
-                            <input type="password" name="password" class="form-control forms_field-input" id="password" placeholder="Contraseña" data-rule="minlen:4" data-msg="Por favor cuatros carateres" />
+                    <div class="space-around my-3">
 
-                            <div class="validate"></div>
+                        <input type="password" name="password" class="form-control forms_field-input" id="password"
+                            placeholder="{{ __('Tu contraseña') }}" data-rule="minlen:4"
+                            data-msg="Por favor cuatros carateres" />
 
-                        </div>
+                        <div class="validate"></div>
 
-                        {{-- Button Login --}}
+                    </div>
 
-                        <button type="submit" class="btn bg-warning">Entrar</button>
+                    {{-- Button Login --}}
+
+                    <button type="submit" class="btn bg-warning">{{ __('Aceptar') }}</button>
 
 
                 </form>
 
                 <p class="my-3">
-                    ¿Aun no eres de los nuestros?
-                    <a href="{{route("register")}}" class="btn ntn-info btn-sm ms-2">
-                       <b>¡Registrate!</b>
+                    {{ __('¿Ya eres de los nuestros?') }}
+                    <a href="{{ route('register') }}" class="btn ntn-info btn-sm ms-2">
+                        <b><button type="button" class="btn btn-outline-dark">{{ __('¡Registrate!') }}</button>
+                        </b>
                     </a>
                 </p>
 
