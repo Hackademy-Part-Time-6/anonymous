5270f2baf0a5a690b18741c263db8d29e7823085
laptopjohn <lebenper@gmail.com>
2023-05-12 19:17:46 +0200

    organizado los errores

--- app/Http/Livewire/CreateAd.php
+++ app/Http/Livewire/CreateAd.php
@@ -52,25 +52,25 @@ class CreateAd extends Component
         $validatedData = $this->validate();
         // busco la categoria
         $category = Category::find($this->category);
-        
+
         // creo el anuncio a partir de la categoria usando la relacion y pasando los datos validados
         $ad = $category->ads()->create($validatedData);
-        
+
         // vuelvo a guardar el anuncio "pasando" por la relacion del usuario
         Auth::user()->ads()->save($ad);
         // guardo cada imagen en el db y en el storage
-        if(count($this->images)){
+        if (count($this->images)) {
             $newFileName = "ads/$ad->id";
             foreach ($this->images as $image) {
                 $newImage = $ad->images()->create([
-                    'path'=>$image->store($newFileName,'public')
+                    'path' => $image->store($newFileName, 'public')
                 ]);
-                dispatch(new ResizeImage($newImage->path,400,300));
+                dispatch(new ResizeImage($newImage->path, 400, 300));
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
-        
-        session()->flash('message',['type'=>'success', 'text'=>'Ad created successfully']);
+
+        session()->flash('message', ['type' => 'success', 'text' => 'Ad created successfully']);
         $this->cleanForm();
     }
 
@@ -87,6 +87,7 @@ class CreateAd extends Component
         $this->body = "";
         $this->category = "";
         $this->price = "";
+        $this->images = [];
     }
 
 
