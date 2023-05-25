81b19bc3d9b1ce6778c91f647585b7b0e02c6750
Douglas Ortega <dortegapuma@gmail.com>
2023-05-25 21:36:19 +0200

    actualización de menú

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -14,7 +14,7 @@
                     <a class="nav-link" href="#">{{ __('Dónde estamos') }}</a>
                 </li> --}}
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('about-us') }}">{{ __('Quienes somos') }}</a>
+                    <a class="nav-link" href="{{ route('about-us') }}">{{ __('Quienes soy') }}</a>
                 </li>
 
                 <li class="nav-item dropdown ">
